1ecdc35c94abf85be31c044362f5f88d36653d02
QouB <racingclub_nico@hotmail.com>
2025-03-17 12:12:01 -0300

    app: New library for Gpio, commented api code for testing purpouses

--- src/app/api/v1/lights/pythonScripts/functionTest.py
+++ src/app/api/v1/lights/pythonScripts/functionTest.py
@@ -4,17 +4,9 @@ import sys
 
 GPIO.setmode(GPIO.BCM)
 
-# GPIO 26 Relay 1
-GPIO.setup(26, GPIO.OUT)
-# GPIO  6 Relay 2
-GPIO.setup(6, GPIO.OUT)
-# GPIO 22 Relay 3
-GPIO.setup(22, GPIO.OUT)
-# GPIO  4 Relay 4
-GPIO.setup(4, GPIO.OUT)
-
 def funcTest(num):
   try:
+    GPIO.setup(num, GPIO.OUT)
     GPIO.output(num, GPIO.HIGH)
     time.sleep(1)
     GPIO.output(num, GPIO.LOW)
