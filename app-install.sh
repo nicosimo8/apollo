ec0ffffafc7c3ac329fe599c177a06915f68f0c3
QouB <racingclub_nico@hotmail.com>
2025-03-25 08:31:49 -0300

    dev: .sh scripts updated

--- app-install.sh
+++ app-install.sh
@@ -32,17 +32,17 @@ sudo usermod -aG docker $USER
 groups $USER
 /usr/bin/newgrp docker<<EONG
 EONG
-echo "******PERMITIENDO A DOCKER, APOLO Y LEDS BOOTEAR DESDE EL INICIO******"
-sudo systemctl enable docker
-sudo systemctl start docker
-# sudo systemctl status docker
-sudo cp apolo.service /etc/systemd/system
-sudo cp estado_led_anodo.service /etc/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl enable estado_led_anodo.service
-sudo systemctl enable test.service
-sudo systemctl enable apolo
-sudo systemctl start estado_led_anodo
-sudo systemctl start test
-sudo systemctl start apolo
+# echo "******PERMITIENDO A DOCKER, APOLO Y LEDS BOOTEAR DESDE EL INICIO******"
+# sudo systemctl enable docker
+# sudo systemctl start docker
+# # sudo systemctl status docker
+# sudo cp apolo.service /etc/systemd/system
+# sudo cp estado_led_anodo.service /etc/systemd/system
+# sudo systemctl daemon-reload
+# sudo systemctl enable estado_led_anodo.service
+# sudo systemctl enable test.service
+# sudo systemctl enable apolo
+# sudo systemctl start estado_led_anodo
+# sudo systemctl start test
+# sudo systemctl start apolo
 }
