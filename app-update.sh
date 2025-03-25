b41118214153e7b0fb55b7953db0653cc420765e
QouB <racingclub_nico@hotmail.com>
2025-03-25 08:48:38 -0300

    dev: .sh scripts updated

--- app-update.sh
+++ app-update.sh
@@ -4,24 +4,24 @@ echo "******TRAYENDO LOS NUEVOS CAMBIOS******"
 git pull
 
 echo "******CHECKEAR LOS CONTAINERS E IMAGENES******"
-docker ps -a
-docker image ls
+sudo docker ps -a
+sudo docker image ls
 
 echo "******DETENIENDO Y ELIMINANDO EL CONTAINER******"
-docker stop apollocontainer
-docker rm apollocontainer
+sudo docker stop apollocontainer
+sudo docker rm apollocontainer
 
 echo "******ELIMINANDO LA IMAGEN******"
-docker image rm apolloimg:v0
+sudo docker image rm apolloimg:v0
 
 echo "******VOLVER A CHECKEAR LOS CONTAINERS E IMAGENES******"
-docker ps -a
-docker image ls
+sudo docker ps -a
+sudo docker image ls
 
 echo "******CREANDO IMAGEN******"
-docker build . -t apolloimg:v0
+sudo docker build . -t apolloimg:v0
 
 echo "******CREANDO Y CORRIENDO EL CONTAINER******"
-docker run --privileged -d --restart=always -v /sys:/sys -p3000:3000 --name apollocontainer -e USERNAME_ONE="argos" -e USERNAME_TWO="argos2" -e USERNAME_THREE="argos3" -e PASSWORD_ONE="three.123" -e PASSWORD_TWO="Argos123" -e PASSWORD_THREE="testing" -e CUSTOMER_NAME="Argos Testing" apolloimg:v0
+sudo docker run --privileged -d --restart=always -v /sys:/sys -p3000:3000 --name apollocontainer -e USERNAME_ONE="argos" -e USERNAME_TWO="argos2" -e USERNAME_THREE="argos3" -e PASSWORD_ONE="three.123" -e PASSWORD_TWO="Argos123" -e PASSWORD_THREE="testing" -e CUSTOMER_NAME="Argos Testing" apolloimg:v0
 # docker run --privileged -v /sys:/sys -p3000:3000 --name apollocontainer apolloimg:v0
 }
