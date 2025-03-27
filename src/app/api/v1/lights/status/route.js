bcddc403fa75e83608b9dc5e36388189cc7dd967
QouB <racingclub_nico@hotmail.com>
2025-03-26 23:03:23 -0300

    api: new get added to status path

--- src/app/api/v1/lights/status/route.js
+++ src/app/api/v1/lights/status/route.js
@@ -1,20 +1,10 @@
 import { NextResponse } from "next/server";
-import { PythonShell } from 'python-shell';
 import { spawn } from 'child_process';
 
-export async function POST(req) {
+export async function GET() {
   try {
     console.log('***Semáforo - INICIO***');
-    console.log('- Consultando pedido');
-
-    const { led, onoff } = await req.json();
-    let state;
-
-    console.log(`- Pedido para GPIO #${led} por estado Estado`);
-
-    // await PythonShell.run(`./src/app/api/v1/lights/status/lightCheck.py`, { args: [led] }, function (err, results) {
-    //   if (err) throw err;
-    // });
+    console.log('- Consultando estados');
 
     const execPython = async (script, args) => {
       const argum = args.map(arg => arg.toString());
@@ -33,10 +23,56 @@ export async function POST(req) {
           reject(`Error ocurred in ${script}`);
         })
 
-        // py.stderr.on("data", (data) => {
-        //   console.error(`[python] Error ocurred: ${data}`);
-        //   reject(`Error ocurred in ${script}`);
-        // })
+        py.on('exit', (code) => {
+          console.log(`Child process exited with code ${code}`);
+          resolve(output);
+        })
+      });
+
+      return result;
+    };
+
+    const data = await execPython('./src/app/api/v1/lights/status/lightsCheck.py');
+
+    console.log('***Semáforo - FIN***');
+
+    return NextResponse.json(
+      { message: "Consulta exitosa", data: data },
+      { status: 200 }
+    );
+  } catch (e) {
+    return NextResponse.json(
+      { message: `Ha habido un error en la consulta: \n ${e}` },
+      { status: 500 }
+    );
+  };
+};
+
+export async function POST(req) {
+  try {
+    console.log('***Semáforo - INICIO***');
+    console.log('- Consultando pedido');
+
+    const { led } = await req.json();
+
+    console.log(`- Pedido para GPIO #${led} por estado Estado`);
+
+    const execPython = async (script, args) => {
+      const argum = args.map(arg => arg.toString());
+
+      const py = spawn("python3", [script, ...argum]);
+
+      const result = await new Promise((resolve, reject) => {
+        let output;
+
+        py.stdout.on('data', (data) => {
+          output = JSON.parse(data);
+        });
+
+        py.stderr.on('error', (err) => {
+          console.error(`[python] Error ocurred: ${err}`);
+          reject(`Error ocurred in ${script}`);
+        })
 
         py.on('exit', (code) => {
           console.log(`Child process exited with code ${code}`);
