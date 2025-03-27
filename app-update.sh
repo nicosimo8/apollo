1487ab96dce82a6f36adbb809f9e063dfdd4e0ab
QouB <racingclub_nico@hotmail.com>
2025-03-27 09:25:56 -0300

    dev: app-update.sh script updated

--- app-update.sh
+++ app-update.sh
@@ -19,11 +19,7 @@ echo "******VOLVER A CHECKEAR LOS CONTAINERS E IMAGENES******"
 sudo docker ps -a
 sudo docker image ls
 
-echo "******CREANDO IMAGEN******"
-sudo docker build . -t apolloimg:v0
+sh app-create.sh
 
-echo "******CREANDO Y CORRIENDO EL CONTAINER******"
-sudo docker run --privileged -d --restart=always -v /sys:/sys -p3000:3000 --name apollocontainer -e USERNAME_ONE="argos" -e USERNAME_TWO="argos2" -e USERNAME_THREE="argos3" -e PASSWORD_ONE="three.123" -e PASSWORD_TWO="Argos123" -e PASSWORD_THREE="testing" -e CUSTOMER_NAME="Argos Testing" apolloimg:v0
-# docker run --privileged -v /sys:/sys -p3000:3000 --name apollocontainer apolloimg:v0
 echo "******APP-UPDATE FIN******"
 }
