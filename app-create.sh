628e06883c17081c0a6ecb3d5cd80ddf80ced9df
QouB <racingclub_nico@hotmail.com>
2025-03-27 09:33:48 -0300

    dev: app-update.sh script updated

--- app-create.sh
+++ app-create.sh
@@ -8,20 +8,20 @@ echo "******CREANDO Y CORRIENDO EL CONTAINER******"
 sudo docker run --privileged -d --restart=always -v /sys:/sys -p3000:3000 --name apollocontainer -e USERNAME_ONE="argos" -e USERNAME_TWO="argos2" -e USERNAME_THREE="argos3" -e PASSWORD_ONE="three.123" -e PASSWORD_TWO="Argos123" -e PASSWORD_THREE="testing" -e CUSTOMER_NAME="Argos Testing" apolloimg:v0
 
 echo "******FINALIZANDO******"
-sudo rm -r readme.md
-sudo rm -r package.json
-sudo rm -r package-lock.json
-sudo rm -r next.config.mjs
-sudo rm -r lock.json
-sudo rm -r jsconfig.json
-sudo rm -r eslint.config.mjs
-sudo rm -r Dockerfile
-sudo rm -r .env
-sudo rm -r src
-sudo rm -r public
-sudo rm -r node_modules
-sudo rm -r build
-sudo rm -r .next
+sudo rm -rf readme.md
+sudo rm -rf package.json
+sudo rm -rf package-lock.json
+sudo rm -rf next.config.mjs
+sudo rm -rf lock.json
+sudo rm -rf jsconfig.json
+sudo rm -rf eslint.config.mjs
+sudo rm -rf Dockerfile
+sudo rm -rf .env
+sudo rm -rf src
+sudo rm -rf public
+sudo rm -rf node_modules
+sudo rm -rf build
+sudo rm -rf .next
 
 echo "******APP-CREATE - FIN******"
 }
