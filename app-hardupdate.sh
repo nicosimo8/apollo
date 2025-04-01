63bee31b4cd922bf15abf96066e9ab2d34dd128f
QouB <racingclub_nico@hotmail.com>
2025-04-01 18:02:19 -0300

    dev: app-hardupdate updated for testing on develop

--- app-hardupdate.sh
+++ app-hardupdate.sh
@@ -2,7 +2,7 @@
 echo "******APP-HARDUPDATE INICIO******"
 echo "******ELIMINANDO******"
 cd ..
-yes | rm -r apollo
+yes | sudo rm -r apollo
 echo "******VOLVIENDO A COPIAR******"
 git clone https://github.com/nicosimo8/apollo.git
 echo "******EJECUTANDO SCRIPT******"
