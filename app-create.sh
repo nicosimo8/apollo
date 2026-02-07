5b65654ceb695879a7309d9fb4e21db7fadb2128
QouB <racingclub_nico@hotmail.com>
2026-02-07 09:03:03 -0300

    dev1.2: api licence added, enviroment variables set to sh create script, licence reader added inside utils

--- app-create.sh
+++ app-create.sh
@@ -12,7 +12,7 @@ sudo docker build . -t apolloimg:v0
 
 echo "******CREANDO Y CORRIENDO LOS CONTAINERS******"
 # sudo docker run --privileged -d --restart=always -v /sys:/sys -p3000:3000 --name apollocontainer --network apolonet -e USERNAME_ONE="admin" -e USERNAME_TWO="operador1" -e USERNAME_THREE="operador2" -e PASSWORD_ONE="admin" -e PASSWORD_TWO="coop.union1" -e PASSWORD_THREE="coop.union2" -e CUSTOMER_NAME="Coop. Unión" apolloimg:v0
-sudo docker run --privileged -d --restart=always -v /sys:/sys -p3000:3000 --name apollocontainer --network apolonet -e USERNAME_ONE=admin -e USERNAME_TWO=operador1 -e USERNAME_THREE=operador2 -e PASSWORD_ONE=admin -e PASSWORD_TWO=coop.union1 -e PASSWORD_THREE=coop.union2 -e CUSTOMER_NAME="Coop. Unión" -e MYSQL_HOST_URL=apolodb -e MYSQL_PORT=3306 -e MYSQL_USER=argos -e MYSQL_PASSWORD=argos -e MYSQL_DATABASE=argosapolodb apolloimg:v0
+sudo docker run --privileged -d --restart=always -v /sys:/sys -p3000:3000 --name apollocontainer --network apolonet -e USERNAME_ONE=admin -e USERNAME_TWO=operador1 -e USERNAME_THREE=operador2 -e PASSWORD_ONE=admin -e PASSWORD_TWO=coop.union1 -e PASSWORD_THREE=coop.union2 -e CUSTOMER_NAME="Coop. Unión" -e MYSQL_HOST_URL=apolodb -e MYSQL_PORT=3306 -e MYSQL_USER=argos -e MYSQL_PASSWORD=argos -e NEXT_API_SECRET_KEY=Tarmo.Goyf -e NEXT_SECRET_KEY=Tarmo.Goyf -e MYSQL_DATABASE=argosapolodb apolloimg:v0
 sudo docker run -d --restart=always -v apolodata:/var/lib/mysql -p3306:3306 --name apolodb --network apolonet -e MYSQL_ROOT_PASSWORD=argos -e MYSQL_USER=argos -e MYSQL_PASSWORD=argos -e MYSQL_DATABASE=argosapolodb mysql
 # sh app-cdb.sh
 
---
+++ src/utils/fileHandle.js
@@ -0,0 +1,16 @@
+'use server'
+
+import fs from 'fs';
+
+export const readFile = async (serial) => {
+  const data = new Promise((resolve, reject) => {
+    fs.readFile(`/home/Argos/${serial}.licence`, 'utf8', (err, data) => {
+      if (err) {
+        reject('Error reading file:', err);
+        return;
+      }
+      resolve(data);
+    });
+  });
+  return data;
+};
